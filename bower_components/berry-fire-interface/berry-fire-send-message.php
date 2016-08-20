70654daabf13d054a61029764ccc9af7a796db7a
teusday <macmaster195@gmail.com>
2016-08-20 18:59:48 -0400

    Merge remote-tracking branch 'berrste/master'
    # Conflicts:
    #	bower_components/iron-image/.bower.json
    #	bower_components/iron-image/bower.json
    #	bower_components/iron-image/iron-image.html

---
+++ bower_components/berry-fire-interface/berry-fire-send-message.php
@@ -0,0 +1,99 @@
+<?php
+$global = new stdClass();
+$auth = json_decode($_REQUEST["auth"]);
+$messageData = json_decode($_REQUEST["messageData"]);
+$options = array('auth' => $auth);
+initCurlHandler();
+setBaseURI("https://cirrus-drive.firebaseio.com");
+$users = json_decode(get("users", $options));
+$id = idExists($messageData->toID, $users);
+if ($id != "null") {
+    $result = json_decode(writeData(getPathFromMessageData($id), $messageData, "POST", $options));
+    if ($result->error) {
+        echo "error";
+    } else {
+        echo "success";
+    }
+} else {
+    echo "user error";
+}
+
+function idExists($check, $users) {
+    $returnMe = "null";
+    foreach($users as $key => $value) {
+        if ($key == $check || $value->username == $check || $value->fullName == $check) {
+            $returnMe = $key;
+        }
+    }
+    return $returnMe;
+}
+
+function initCurlHandler() {
+        $GLOBALS['global']->_curlHandler = curl_init();
+}
+
+function closeCurlHandler() {
+        curl_close($GLOBALS['global']->_curlHandler);
+}
+
+function setBaseURI($baseURI) {
+        $baseURI .= (substr($baseURI, -1) == '/' ? '' : '/');
+        $GLOBALS['global']->_baseURI = $baseURI;
+}
+
+function get($path, $options = array()) {
+        try {
+            $ch = _getCurlHandler($path, 'GET', $options);
+            $return = curl_exec($ch);
+        } catch (Exception $e) {
+            $return = null;
+        }
+        return $return;
+}
+
+function writeData($path, $data, $method = 'POST', $options = array()) {
+        $jsonData = json_encode($data);
+        $header = array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($jsonData)
+        );
+        try {
+            $ch = _getCurlHandler($path, $method, $options);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+            $return = curl_exec($ch);
+        } catch (Exception $e) {
+            $return = null;
+        }
+        return $return;
+}
+
+function getPathFromMessageData($id) {
+	return "messages/" . $id . "/inbox";
+}
+
+function _getJsonPath($path, $options = array()) {
+        $url = $GLOBALS['global']->_baseURI;
+        if ($GLOBALS['global']->_token !== '') {
+            $options['auth'] = $GLOBALS['global']->_token;
+        }
+        $path = ltrim($path, '/');
+        return $url . $path . '.json?' . http_build_query($options);
+    }
+
+function _getCurlHandler($path, $mode, $options = array()) {
+        $url = _getJsonPath($path, $options);
+        $ch = $GLOBALS['global']->_curlHandler;
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $GLOBALS['global']->_timeout);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $GLOBALS['global']->_timeout);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $mode);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        return $ch;
+    }
+
+
+
+?>
--- readme.md
+++ readme.md
@@ -4,9 +4,10 @@
 **To-Do:**
 - Change all the headers to paper-headers
 - Messages
-  - make reply work
-  - create _new message_ button
-  - create _new message_ screen
+  - ~~make reply work~~
+  - ~~create _new message_ button~~
+  - ~~create _new message_ screen~~
+  - add _no messages_ screen
 - Projects
   - make _closed projects_ page
   - make button to submit, edit, and delete a project
